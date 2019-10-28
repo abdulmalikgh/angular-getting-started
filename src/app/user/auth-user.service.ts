7ba0b9e637ec6d5802c600b8416f55bf29e107e9
abdulmalikgh <50835233+abdulmalikgh@users.noreply.github.com>
2019-10-27 23:58:54 -0700

    added message component

--- src/app/user/auth-user.service.ts
+++ src/app/user/auth-user.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@angular/core';
+import { User } from './user'
 
 @Injectable()
 export class AuthUserService {
-
+  currentUser : User;
+  
+  get isLoggedIn():boolean{
+    return !!this.currentUser;
+  }
   constructor() { }
+  loging(userName:string, password:string):void{
+    if(!userName || !password){
+      this.m
+    }
+  }
 }
